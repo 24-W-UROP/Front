345081b56f6b5caf33764ee5f1c999362042bb0d
qkrwlalss <128897708+qkrwlalss@users.noreply.github.com>
2024-01-13 01:34:38 +0900

    feat: 버튼 컴포넌트

---
+++ src/components/button/styles.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+export const BigButton = styled.button`
+  width: 120px;
+  height: 64px;
+  border: none;
+  border-radius: 10px;
+  background: #92c2b1;
+  font-size: 16px;
+  font-weight: 600;
+  color: white;
+  cursor: pointer;
+  padding: 4px;
+  margin: 12px;
+`;
+
+export const SmallButton1 = styled.button`
+  width: 88px;
+  height: 40px;
+  border: 3px solid white;
+  border-radius: 10px;
+  background: #92c2b1;
+  font-size: 14px;
+  font-weight: 600;
+  color: white;
+  cursor: pointer;
+  padding: 4px;
+  margin: 12px;
+`;
+
+export const SmallButton2 = styled.button`
+  width: 88px;
+  height: 40px;
+  border: 2px solid #92c2b1;
+  border-radius: 10px;
+  background: white;
+  font-size: 14px;
+  font-weight: 600;
+  color: #92c2b1;
+  cursor: pointer;
+  padding: 4px;
+  margin: 12px;
+`;
